dcd2394056ac8ba57021571b65ebc85eb232c054
alikariminik <alirkariminik@gmail.com>
2023-05-25 19:17:34 +0000

    initial commit

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="">
+        hello world
+      </div>
+    </div>
+  )
+}
