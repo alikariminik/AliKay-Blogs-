a1875edaab8bee9ec3a6f7d7b37b01d246164bbe
alikariminik <alirkariminik@gmail.com>
2023-05-25 23:09:13 +0000

    feature: (header) created header layout

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,16 +1,32 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import 'tailwindcss/tailwind.css'
+import { PostCard, Categories, PostWidget, Header } from '../components';
+
+const posts = [
+  {title: 'React Testing;', excerpt: 'Learn React Testing'},
+  {title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'},
+]
 
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>ARKN Blogs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="">
-        hello world
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1 text-rose-700">
+            {posts.map((post, index) => 
+              <PostCard post={post} key={post.title} />
+            )},
+        </div>
+        <div className="lg:col-span-4 col-span-1 text-rose-700	">
+              <div className="lg:sticky relativ top-8">
+                <PostWidget />
+                <Categories />
+              </div>
+        </div>
       </div>
     </div>
   )
